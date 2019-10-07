e38d6a7279af8482613df727578c7c9ef5db1c2b
Satyajit Ghana <satyajitghana7@gmail.com>
2019-10-07 21:14:00 +0530

    Update linked_list.cpp

--- linked_list/cpp/linked_list.cpp
+++ linked_list/cpp/linked_list.cpp
@@ -3,14 +3,16 @@
 struct Node { 
    int data; 
    struct Node *next; 
-}; 
+};
+
 struct Node* head = NULL;   
 void insert(int new_data) { 
    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
    new_node->data = new_data; 
    new_node->next = head; 
    head = new_node; 
-} 
+}
+
 void display() { 
    struct Node* ptr;
    ptr = head;
@@ -18,7 +20,8 @@ void display() {
       std::cout<< ptr->data <<" "; 
       ptr = ptr->next; 
    } 
-} 
+}
+
 int main() { 
    insert(3);
    insert(1);

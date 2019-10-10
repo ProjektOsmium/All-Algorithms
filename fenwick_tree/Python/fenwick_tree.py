df9a4c00de74542f612a90c84dbc30bc3c95073a
Mohammed Coovadia <mohammed@coovadia.com>
2019-10-10 13:37:33 -0400

    refactored and updated algorithm with a test array

--- fenwick_tree/Python/fenwick_tree.py
+++ fenwick_tree/Python/fenwick_tree.py
@@ -1,77 +1,65 @@
-#!/usr/bin/env python3
+# Python implementation of Binary Indexed Tree
 
-"""
-Binary Indexed Tree / Fenwick Tree
-https://www.hackerearth.com/practice/notes/binary-indexed-tree-made-easy-2/
-https://www.topcoder.com/community/data-science/data-science-tutorials/binary-indexed-trees/
-https://www.youtube.com/watch?v=v_wj_mOAlig
-https://www.youtube.com/watch?v=kPaJfAUwViY
-"""
+# Returns sum of arr[0..index]. This function assumes
+# that the array is preprocessed and partial sums of
+# array elements are stored in BITree[].
+def getsum(BITTree,i):
+    s = 0 #initialize result
 
+    # index in BITree[] is 1 more than the index in arr[]
+    i = i+1
 
-def update(index, value, array, bi_tree):
-	"""
-	Updates the binary indexed tree with the given value
-	:param index: index at which the update is to be made
-	:param value: the new element at the index
-	:param array: the input array
-	:param bi_tree: the array representation of the binary indexed tree
-	:return: void
-	"""
+    # Traverse ancestors of BITree[index]
+    while i > 0:
 
-	while index < len(array):
-		bi_tree[index] += value
-		index += index & -index
+        # Add current element of BITree to sum
+        s += BITTree[i]
 
+        # Move index to parent node in getSum View
+        i -= i & (-i)
+    return s
 
-def get_sum(index, bi_tree):
-	"""
-	Calculates the sum of the elements from the beginning to the index
-	:param index: index till which the sum is to be calculated
-	:param bi_tree: the array representation of the binary indexed tree
-	:return: (integer) sum of the elements from beginning till index
-	"""
-	ans = 0
+# Updates a node in Binary Index Tree (BITree) at given index
+# in BITree. The given value 'val' is added to BITree[i] and
+# all of its ancestors in tree.
+def updatebit(BITTree , n , i ,v):
 
-	while index > 0:
-		ans += bi_tree[index]
-		index -= index & -index
+    # index in BITree[] is 1 more than the index in arr[]
+    i += 1
 
-	return ans
+    # Traverse all ancestors and add 'val'
+    while i <= n:
+
+        # Add 'val' to current node of BI Tree
+        BITTree[i] += v
+
+        # Update index to that of parent in update View
+        i += i & (-i)
 
 
-def get_range_sum(left, right, bi_tree):
-	"""
-	Calculates the sum from the given range
+# Constructs and returns a Binary Indexed Tree for given
+# array of size n.
+def construct(arr, n):
 
-	:param bi_tree: the array representation of the binary indexed tree
-	:param left: left index of the range (1-indexed)
-	:param right: right index of the range (1-indexed)
-	:return: (integer) sum of the elements in the range
-	"""
-	ans = get_sum(right, bi_tree) - get_sum(left - 1, bi_tree)
+    # Create and initialize BITree[] as 0
+    BITTree = [0]*(n+1)
 
-	return ans
+    # Store the actual values in BITree[] using update()
+    for i in range(n):
+        updatebit(BITTree, n, i, arr[i])
 
-
-def main():
-	n = int(input('Enter the number of elements: '))
-	arr = [int(x) for x in input('Enter the {} elements of the array: '.format(n)).split()]
-	arr.insert(0, 0)   # insert dummy node for 1-based indexing
-	bit = [0 for i in range(n+1)]
-
-	for index in range(1, n+1):
-		update(index, arr[index], arr, bit)
-
-	"""
-	For range sum queries
-		l, r = map(int, input('Enter the left and right indices for the range sum: ').split())
-		print(get_range_sum(l, r, bit))
-
-	For updating the binary indexed tree
-		update(index, new_value - arr[index], arr, bit)
-	"""
+    # Uncomment below lines to see contents of BITree[]
+    #for i in range(1,n+1):
+    #     print BITTree[i],
+    return BITTree
 
 
-if __name__ == '__main__':
-	main()
+# Driver code to test above methods
+freq = [2, 1, 1, 3, 2, 3, 4, 5, 6, 7, 8, 9]
+print(' '.join(map(str, freq)))
+BITTree = construct(freq,len(freq))
+print("Sum of elements in arr[0..5] is " + str(getsum(BITTree,5)))
+freq[3] += 6
+updatebit(BITTree, len(freq), 3, 6)
+print("Sum of elements in arr[0..5]"+
+                    " after update is " + str(getsum(BITTree,5)))

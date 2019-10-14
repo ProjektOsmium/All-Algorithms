e6c96b907cf13a2008759758a159592a8e28d142
Siwat Jirawiwatpat <bank2u@yahoo.com>
2019-10-14 12:28:28 +0700

    Add CSharp sort algorithm

---
+++ merge_sort/CSharp/merge_sort.cs
@@ -0,0 +1,79 @@
+using System;
+namespace QuickSortDemo
+{
+    class Example
+    {
+        static public void merge(int[] arr, int p, int q, int r)
+        {
+            int i, j, k;
+            int n1 = q - p + 1;
+            int n2 = r - q;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            for (i = 0; i < n1; i++)
+            {
+                L[i] = arr[p + i];
+            }
+            for (j = 0; j < n2; j++)
+            {
+                R[j] = arr[q + 1 + j];
+            }
+            i = 0;
+            j = 0;
+            k = p;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        static public void mergeSort(int[] arr, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = (p + r) / 2;
+                mergeSort(arr, p, q);
+                mergeSort(arr, q + 1, r);
+                merge(arr, p, q, r);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 76, 89, 23, 1, 55, 78, 99, 12, 65, 100 };
+            int n = 10, i;
+            Console.WriteLine("Merge Sort");
+            Console.Write("Initial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            mergeSort(arr, 0, n - 1);
+            Console.Write("\nSorted Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}

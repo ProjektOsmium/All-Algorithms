88398dbbec3f1577ad7b81e050038b9fb1d4aca1
Nikhil3698 <54366846+Nikhil3698@users.noreply.github.com>
2020-10-02 22:07:38 +0530

    Added a C++ file for binary search tree
    This is the c++ implementation for Binary Search Tree .

---
+++ binary_search_tree/C++/binary_search_tree.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node{
+public:
+	int data;
+	node* left;
+	node* right;
+	node(int d){
+		data =d;
+		left = right=NULL;
+	}
+};
+
+node* InsertInBst(node* root,int data){
+	if(root == NULL){
+		root = new node(data);
+		return root;
+	}
+
+	if(data<=root->data){
+		root->left = InsertInBst(root->left,data);
+	}
+	else{
+		root->right = InsertInBst(root->right,data);
+	}
+	return root;
+}
+
+node* CreateBST(){
+	node* root = NULL;
+	int data;
+	cin>>data;
+	while(data!=-1){
+		root = InsertInBst(root,data);
+
+		cin>>data;
+	}
+	return root;
+}
+
+void PreOrder(node* root){
+	if(root == NULL){
+		return;
+	}
+
+	cout<<root->data<<' ';
+	PreOrder(root->left);
+	PreOrder(root->right);
+}
+
+
+void InOrder(node* root){
+	if(root == NULL){
+		return;
+	}
+
+	InOrder(root->left);
+	cout<<root->data<<' ';
+	InOrder(root->right);
+}
+
+
+void PostOrder(node* root){
+	if(root == NULL){
+		return;
+	}
+
+	PostOrder(root->left);
+	PostOrder(root->right);
+	cout<<root->data<<' ';
+}
+
+void LevelOrder(node* root){
+	queue<node*> q;
+	q.push(root);
+	q.push(NULL);
+	while(!q.empty()){
+		node* ans = q.front();
+		q.pop();
+		if(ans == NULL){
+			cout<<endl;
+			if(!q.empty()){
+				q.push(NULL);
+			}
+		}
+		else{
+			cout<<ans->data<<" ";
+			if(ans->left){
+				q.push(ans->left);
+			}
+			if(ans->right){
+				q.push(ans->right);
+			}
+		}
+	}
+}
+
+void Print(node* head){
+	while(head){
+		cout<<head->data<<"-->";
+		head = head->right;
+	}
+	cout<<"NULL"<<endl;
+}
+
+int main(){
+	node* root = NULL;
+	root = CreateBST();
+	
+	 PreOrder(root);
+	 cout<<endl;
+	 InOrder(root);
+	 cout<<endl;
+	 PostOrder(root);
+	 cout<<endl;
+	 LevelOrder(root);
+	return 0;
+}
